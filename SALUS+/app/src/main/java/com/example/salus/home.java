66b5617893075feb7d2f9970f737088f3f305f98
unknown <bnc1423@gmail.com>
2023-09-28 23:30:18 -0300

    Agrego parte del home a Android Studio

--- SALUS+/app/src/main/java/com/example/salus/home.java
+++ SALUS+/app/src/main/java/com/example/salus/home.java
@@ -3,16 +3,16 @@ package com.example.salus;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+
 import android.widget.Button;
 
-public class MainActivity extends AppCompatActivity {
+
+public class home extends AppCompatActivity {
 
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
-
-
+        setContentView(R.layout.activity_home);
     }
 }
--- SALUS+/app/src/main/java/com/example/salus/login.java
+++ SALUS+/app/src/main/java/com/example/salus/login.java
@@ -3,12 +3,15 @@ package com.example.salus;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.Button;
 
 public class login extends AppCompatActivity {
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+
     }
 }
