afe18c47eaca9df3ef4208ac9bca8cee86d65196
Joselo Esteban Orellano <mobilelegendsbang2020bang@gmail.com>
2023-09-30 06:52:45 -0300

    #TK25 Activity Profesionales realizada y se aplico navegabilidad entre algunas activities MainActivity , login, home, Profesionales

--- SALUS+/app/src/main/java/com/example/salus/MainActivity.java
+++ SALUS+/app/src/main/java/com/example/salus/MainActivity.java
@@ -2,17 +2,22 @@ package com.example.salus;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
-import android.widget.Button;
+import android.view.View;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
-
+    private TextView txtHolaMundo;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-
-
+        this.txtHolaMundo = findViewById(R.id.holaMundo);
+    }
+    public void irLogin(View view){
+        Intent intent = new Intent(this,login.class);
+        startActivity(intent);
     }
 }
