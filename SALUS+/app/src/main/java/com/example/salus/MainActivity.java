48c1dd4d1ef1055ce3dcdf6bc3549a29c6609bcb
Gonzalo Alvarado <gonalvarado86@gmail.com>
2023-09-30 09:51:06 -0300

    subo modificaciones a la interfaz MainActivity

--- SALUS+/app/src/main/java/com/example/salus/MainActivity.java
+++ SALUS+/app/src/main/java/com/example/salus/MainActivity.java
@@ -8,16 +8,10 @@ import android.view.View;
 import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
-    private TextView txtHolaMundo;
-
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        this.txtHolaMundo = findViewById(R.id.holaMundo);
     }
-    public void irLogin(View view){
-        Intent intent = new Intent(this,login.class);
-        startActivity(intent);
-    }
+
 }
