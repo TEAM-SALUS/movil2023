581c29e18d5a6b32e27a315da2fa7a334482eece
pululon <pululon@hotmail.com>
2023-09-30 21:35:31 -0300

    Agregada la Activity Profesional

--- SALUS+/app/src/main/java/com/example/salus/ProfesionalActivity.java
+++ SALUS+/app/src/main/java/com/example/salus/ProfesionalActivity.java
@@ -1,2 +1,15 @@
-package com.example.salus;public class ProfesionalActivity {
+package com.example.salus;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+
+public class ProfesionalActivity extends AppCompatActivity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_profesional);
+    }
+
 }
--- SALUS+/app/src/main/java/com/example/salus/ProfesionalesActivity.java
+++ SALUS+/app/src/main/java/com/example/salus/ProfesionalesActivity.java
@@ -2,7 +2,9 @@ package com.example.salus;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 
 public class ProfesionalesActivity extends AppCompatActivity {
 
@@ -10,5 +12,10 @@ public class ProfesionalesActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_profesionales);
+    }
+    public void irProfesional(View view) {
+        Intent intent = new Intent(this, ProfesionalActivity.class);
+        startActivity(intent);
+
     }
 }
