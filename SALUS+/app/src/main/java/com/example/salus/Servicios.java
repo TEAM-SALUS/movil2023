9b7473ade61797d7f94ef7ca1050414cb28559ae
Zian <cam.iva@hotmail.com>
2023-10-08 21:34:17 -0300

    Activity servicios

---
+++ SALUS+/app/src/main/java/com/example/salus/Servicios.java
@@ -0,0 +1,28 @@
+package com.example.salus;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+public class Servicios extends AppCompatActivity {
+
+    private Button reservarTurno;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_servicios);
+
+        reservarTurno = findViewById(R.id.reservarTurno1);
+    }
+
+        /*
+    public void irConfirmarServicio(View view){
+        Intent intent = new Intent(this, ConfirmarServicioActivity.class);
+        startActivity(intent);
+    }
+         */
+}
