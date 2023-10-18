ac91084eacaf843557dd7bef897f037a3f281761
unknown <bnc1423@gmail.com>
2023-10-17 21:50:02 -0300

    actualizo mi rama

--- SALUS+/app/src/main/java/com/example/salus/ServiciosActivity.java
+++ SALUS+/app/src/main/java/com/example/salus/ServiciosActivity.java
@@ -16,16 +16,16 @@ public class ServiciosActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_servicios);
 
-        reservarTurno = (Button)findViewById(R.id.reservarTurno1);
+        /*reservarTurno = (Button)findViewById(R.id.reservarTurno1);
         reservarTurno.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
                Intent i = new Intent(createPackageContext(ServiciosActivity.this, ConfirmarTurnoActivity.class));
             }
-        });
+        });*/
     }
 
-    private Intent createPackageContext(ServiciosActivity serviciosActivity, Class<ConfirmarTurnoActivity> confirmarTurnoActivityClass) {
+    /*private Intent createPackageContext(ServiciosActivity serviciosActivity, Class<ConfirmarTurnoActivity> confirmarTurnoActivityClass) {
         return null;
-    }
+    }*/
 }
