409ba7d7029c270af589606ed532c2d3f3348795
unknown <bnc1423@gmail.com>
2023-10-29 20:32:35 -0300

    Preparando rama

--- SALUS+/app/src/main/java/com/example/salus/ServiciosOdon.java
+++ SALUS+/app/src/main/java/com/example/salus/ServiciosOdon.java
@@ -2,13 +2,21 @@ package com.example.salus;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
-public class ServiciosOdon extends AppCompatActivity {
+public class ServiciosOdon extends AppCompatActivity
+{
+
+
+    Button ReservaT;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_servicios_odon);
     }
+
 }
