15d77ffb55f422138f9d2a3eebdad4688f049d62
RosanaQ <rosanagq@gmail.com>
2023-10-29 22:34:37 -0300

    se genera variable global para CRUD

--- SALUS+/app/src/main/java/com/example/salus/login.java
+++ SALUS+/app/src/main/java/com/example/salus/login.java
@@ -4,6 +4,7 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Context;
 import android.content.Intent;
+import android.content.SharedPreferences;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
@@ -23,6 +24,12 @@ public class login extends AppCompatActivity implements View.OnClickListener{
     IUsuarioNeg iUsuarioNeg;
     String email;
     String pass;
+
+    public static final String DNI_CLIENT="DNICLIENTE";
+    public static final String DNI_PROFESIONAL="DNIPROFECIONAL";
+    public static final String COD_SERVICIO="CODSERVICIOS";
+    public static final String FECHA_TURNO="FECHATURNO";
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -56,7 +63,11 @@ public class login extends AppCompatActivity implements View.OnClickListener{
         if (res >= 0){
             Toast.makeText(context, "Usuario logueado", Toast.LENGTH_LONG).show();
             Intent intent = new Intent(this, home.class);
-            intent.putExtra("dniCliente", res);
+            //intent.putExtra("dniCliente", res);
+            SharedPreferences sharedpreferences = getSharedPreferences("shared_login_data",   Context.MODE_PRIVATE);
+            SharedPreferences.Editor editor = sharedpreferences.edit();
+            editor.putInt(DNI_CLIENT,user.getDni());
+            editor.commit();
             startActivity(intent);
         }else{
             Toast.makeText(context, "Usuario y contraseña incorrectos", Toast.LENGTH_LONG).show();
