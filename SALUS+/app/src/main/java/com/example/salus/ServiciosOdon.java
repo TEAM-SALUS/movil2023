ae7b68c63b456bb27bc9104351fa36e20138c798
unknown <bnc1423@gmail.com>
2023-11-11 03:13:52 -0300

    Modificacion y lista de profesionales en activity ServicioOdon

--- SALUS+/app/src/main/java/com/example/salus/ServiciosOdon.java
+++ SALUS+/app/src/main/java/com/example/salus/ServiciosOdon.java
@@ -7,16 +7,103 @@ import android.content.Intent;
 import android.content.SharedPreferences;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
 import android.widget.Button;
+import android.widget.Spinner;
+import android.widget.Toast;
+
+import java.util.ArrayList;
 
 public class ServiciosOdon extends AppCompatActivity implements View.OnClickListener{
     Button ReservaT;
+    private Spinner spinner;
+    private Button boton;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_servicios_odon);
-        ReservaT = findViewById(R.id.IdBtnReserva);
+        ReservaT = findViewById(R.id.BtnReserva);
+
+        //inicializar componentes
+        spinner = findViewById(R.id.spinnerProf);
+        boton = findViewById(R.id.button);
+        ReservaT.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent i = new Intent(ServiciosOdon.this,TurneroActivity.class);
+                startActivity(i);
+            }
+        });
+
+        //Ejecutar al principio
+        llenarSpinner();
+
+        //Acciones del Boton
+        boton.setOnClickListener(new View.OnClickListener(){
+
+            @Override
+            public void onClick(View v) {
+                mostrarIdSeleccionado();
+            }
+        });
+
+        ArrayList<Profesional> profesional= new ArrayList<>();
+        profesional.add(new Profesional( 1, "Mariano", "Rodriguez"));
+        profesional.add(new Profesional( 2, "Claudio", "Quinteros"));
+        profesional.add(new Profesional( 3, "Josefina", "Miranda"));
+        profesional.add(new Profesional( 4, "Pedro", "Bustamante"));
+        profesional.add(new Profesional( 5, "Ariel", "Romero"));
+        profesional.add(new Profesional( 6, "Milagros", "Sanchez"));
+        profesional.add(new Profesional( 7, "Martín", "Moreno"));
+        profesional.add(new Profesional( 8, "Eugenia", "Ordoñez"));
+        profesional.add(new Profesional( 9, "Paatricia", "López"));
+        profesional.add(new Profesional( 10, "Gabriela", "López"));
+
+        ArrayAdapter<Profesional> adapter = new ArrayAdapter<>(this, androidx.appcompat.R.layout.support_simple_spinner_dropdown_item,profesional);
+
+        spinner.setAdapter(adapter);
+
+        //a la hora de cambiar el objeto nos realiza la siguiente accion
+        spinner.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
+            @Override
+            public void onItemSelected(AdapterView<?> parent, View view, int position, long id) {
+                Toast.makeText(ServiciosOdon.this, "Realice una selección", Toast.LENGTH_SHORT).show();
+            }
+
+            @Override
+            public void onNothingSelected(AdapterView<?> parent) {
+
+            }
+        });
+
+    }
+
+    private void mostrarIdSeleccionado() {
+        Profesional profesional = (Profesional) spinner.getSelectedItem();
+        String mensaje = "El ID seleccionado es "+profesional.getId()+"y pertenece al Profesional "+profesional.getNombre();
+        Toast.makeText(this, mensaje, Toast.LENGTH_SHORT).show();
+    }
+
+    private void llenarSpinner() {
+        ArrayList<Profesional> profesional= new ArrayList<>();
+        profesional.add(new Profesional( 1, "Mariano", "Rodriguez"));
+        profesional.add(new Profesional( 2, "Claudio", "Quinteros"));
+        profesional.add(new Profesional( 3, "Josefina", "Miranda"));
+        profesional.add(new Profesional( 4, "Pedro", "Bustamante"));
+        profesional.add(new Profesional( 5, "Ariel", "Romero"));
+        profesional.add(new Profesional( 6, "Milagros", "Sanchez"));
+        profesional.add(new Profesional( 7, "Martín", "Moreno"));
+        profesional.add(new Profesional( 8, "Eugenia", "Ordoñez"));
+        profesional.add(new Profesional( 9, "Paatricia", "López"));
+        profesional.add(new Profesional( 10, "Gabriela", "López"));
+
+        ArrayAdapter<Profesional> adapter = new ArrayAdapter<>(this, androidx.appcompat.R.layout.support_simple_spinner_dropdown_item,profesional);
+
+        spinner.setAdapter(adapter);
+
+
     }
 
     private void irTurnero(View view){
--- SALUS+/app/src/main/java/com/example/salus/Turnos.java
+++ SALUS+/app/src/main/java/com/example/salus/Turnos.java
@@ -2,13 +2,63 @@ package com.example.salus;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.ContentValues;
+import android.content.Context;
+import android.database.sqlite.SQLiteDatabase;
+import android.database.sqlite.SQLiteOpenHelper;
 import android.os.Bundle;
 
-public class Turnos extends AppCompatActivity {
+import com.example.salus.entidad.Turno;
+
+import java.util.ArrayList;
+import java.util.List;
+
+/*public class Turnos extends AppCompatActivity {
+
+    MyDatabaseHelper dbHelper = new MyDatabaseHelper(this);
+
+    SQLiteDatabase db = dbHelper.getWritableDatabase();
+
+
+    public class MyDatabaseHelper extends SQLiteOpenHelper {
+        private static final int DATABASE_VERSION = 1;
+        private static final String DATABASE_NAME = "MyDatabase";
+
+        private static final String TABLE_NAME = "MyTable";
+        private static final String COLUMN_ID = "id";
+        private static final String COLUMN_NAME = "name";
+
+        public MyDatabaseHelper(Context context) {
+            super(context, DATABASE_NAME, null, DATABASE_VERSION);
+            dbHelper = new MyDatabaseHelper(this);
+            dbHelper = new MyDatabaseHelper(this);
+        }
+
+        @Override
+        public void onCreate(SQLiteDatabase db) {
+            String CREATE_TABLE = "CREATE TABLE " + TABLE_NAME + "("
+                    + COLUMN_ID + " INTEGER PRIMARY KEY AUTOINCREMENT,"
+                    + COLUMN_NAME + " TEXT"
+                    + ")";
+            db.execSQL(CREATE_TABLE);
+        }
+
+
+
+        @Override
+        public void onUpgrade(SQLiteDatabase db, int oldVersion, int newVersion) {
+            db.execSQL("DROP TABLE IF EXISTS " + TABLE_NAME);
+            onCreate(db);
+        }
+
+    }
+
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_turnos);
     }
-}
+
+}*/
