9287ed37c3affe970539ffb109f1f6af47e47f19
Zian <cam.iva@hotmail.com>
2023-11-14 21:42:12 -0300

    Confirmación del mensaje enviado y volver al home

--- SALUS+/app/src/main/java/com/example/salus/ContactoMensajeEnviado.java
+++ SALUS+/app/src/main/java/com/example/salus/ContactoMensajeEnviado.java
@@ -2,13 +2,26 @@ package com.example.salus;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.annotation.SuppressLint;
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class ContactoMensajeEnviado extends AppCompatActivity {
+    Button volver;
 
+    @SuppressLint("MissingInflatedId")
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_contacto_mensaje_enviado);
+
+        volver = findViewById(R.id.volverHome);
+    }
+
+    public void irHome(View view){
+        Intent intent = new Intent(this, home.class);
+        startActivity(intent);
     }
 }
