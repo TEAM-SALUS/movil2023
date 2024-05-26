869756164c7e55de895bde924e749c6708a88696
Vero <veronievasguardia@gmail.com>
2024-05-26 14:29:08 -0300

    TK3 - conectar activity especialidades con el endpoint django ESPECIALIDADES

--- SALUS+/app/src/main/java/com/example/salus/ApiDjango.java
+++ SALUS+/app/src/main/java/com/example/salus/ApiDjango.java
@@ -1,12 +1,16 @@
 package com.example.salus;
 
 import com.example.salus.entidad.Autorizacion;
+import com.example.salus.entidad.Especialidad;
 import com.example.salus.entidad.Usuario;
+
+import java.util.List;
 
 import retrofit2.Call;
 import retrofit2.http.Field;
 import retrofit2.http.FormUrlEncoded;
 import retrofit2.http.POST;
+import retrofit2.http.GET;
 
 public interface ApiDjango {
     @FormUrlEncoded
@@ -15,4 +19,6 @@ public interface ApiDjango {
       @Field("username") String username,
       @Field("password") String password
     );
+    @GET("especialidad")
+    Call<List<Especialidad>> getEspecialidades();
 }
