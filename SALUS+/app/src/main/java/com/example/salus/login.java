4ba1e142ecff2b22414c288cfa421887b40b48f9
isaias rub <106613100+isaias4912@users.noreply.github.com>
2024-05-26 21:04:00 -0300

    subiendo sing-up

--- SALUS+/app/src/main/java/com/example/salus/login.java
+++ SALUS+/app/src/main/java/com/example/salus/login.java
@@ -42,7 +42,7 @@ public class login extends AppCompatActivity {
                 OkHttpClient.Builder httpClient = new OkHttpClient.Builder();
                 httpClient.addInterceptor(loggin);
                 Retrofit retrofit = new Retrofit.Builder()
-                        .baseUrl("http://192.168.1.6:8000/api/v1/")
+                        .baseUrl("http://192.168.0.110:8000/api/v1/")
                         .addConverterFactory(GsonConverterFactory.create())
                         .client(httpClient.build())
                         .build();
