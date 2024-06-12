5f5327f94e35b8347facb36dd7bb87a17f150b06
ElAlef <ben-ale@hotmail.com>
2024-06-12 15:29:51 -0300

    actualizando .idea y ipv4

--- SALUS+/app/src/main/java/com/example/salus/dao/URLConection.java
+++ SALUS+/app/src/main/java/com/example/salus/dao/URLConection.java
@@ -9,8 +9,8 @@ public abstract class URLConection {
 
     //public final static String URLPrivada = "http://192.168.0.44:8000/api/v1/";
    // public final static String URLPrivada = "http://192.168.1.92:8000/api/v1/";
-    public final static String URLPrivada = "http://192.168.1.18:8000/api/v1/";
+    //public final static String URLPrivada = "http://192.168.1.18:8000/api/v1/";
     //public final static String URLPrivada = "http://192.168.56.1:8000/api/v1/";
-
+    public final static String URLPrivada = "http://192.168.1.12:8000/api/v1/";
 
 }
